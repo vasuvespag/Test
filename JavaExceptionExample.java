3e5a149c8363dfc754d5f41649445c4aaf540632
vasuvespag <vasu.vespag@gmail.com>
2022-03-14 10:59:14 +0530

    JavaExceptionExample.java

---
+++ JavaExceptionExample.java
@@ -0,0 +1,27 @@
+package test;
+
+public class JavaExceptionExample{  
+	public static void main(String[] args) {  
+        
+        try{    
+             int a[]=new int[5];    
+             //a[5]=30/0;    
+             System.out.println(a[10]);  
+            }    
+        
+        catch(Exception e)  
+        {  
+         System.out.println("Parent Exception occurs");  
+        }   
+            catch(ArithmeticException e)  
+               {  
+                System.out.println("Arithmetic Exception occurs");  
+               }    
+            /*catch(ArrayIndexOutOfBoundsException e)  
+               {  
+                System.out.println("ArrayIndexOutOfBounds Exception occurs");  
+               }    */
+                      
+            System.out.println("rest of the code");    
+ }  
+}  
