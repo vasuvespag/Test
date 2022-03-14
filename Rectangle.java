95c3c0ffa7196fd84cdf9773899f438479b5e721
vasuvespag <vasu.vespag@gmail.com>
2022-03-14 11:14:27 +0530

    Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Rectangle extends Shape{
+	int length = 10;
+	int breadth = 7;
+	
+	void area()
+	{
+		System.out.println("The area of Rectangle of Length "+length+ " and breadth "+breadth+" is "+length*breadth);
+	}
+
+}
